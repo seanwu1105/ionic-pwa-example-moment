9ad7d80273a99d61075eec0ce526f333695c6e4d
Sean Wu <seanwu1105@gmail.com>
2021-01-31 23:25:39 +0800

    Enable PWA.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-- [ ] PWA
+- [x] PWA
+- [ ] system optimize
+  - AOT
+  - Strict template check
+  - production build
+- [ ] CD PWA on GitHub page
 - [ ] UI mock
+- [ ] App shell
 - [ ] Camera view
 - [ ] RxDB
 - [ ] NgRx/data
+
+### Live-reload PWA
+
+- `npx ng build --prod --watch`
+- `lite-server --baseDir="www"`
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
