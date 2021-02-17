cf580d8dd25ca99e90590be6d1deb7e07609375e
Sean Wu <seanwu1105@gmail.com>
2021-02-17 15:59:43 +0800

    Implement fullscreen image viewer.

--- src/app/features/home/photo/photo.module.ts
+++ src/app/features/home/photo/photo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { NgxIonicImageViewerModule } from 'ngx-ionic-image-viewer';
 import { SharedModule } from '../../../shared/shared.module';
 import { PhotoPageRoutingModule } from './photo-routing.module';
 import { PhotoPage } from './photo.page';
 
 @NgModule({
-  imports: [SharedModule, PhotoPageRoutingModule],
+  imports: [SharedModule, PhotoPageRoutingModule, NgxIonicImageViewerModule],
   declarations: [PhotoPage],
 })
 export class PhotoPageModule {}
