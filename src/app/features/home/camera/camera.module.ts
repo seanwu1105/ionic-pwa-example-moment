4d9136ec5451c35953a3b1673dc9f014f61408d3
Sean Wu <seanwu1105@gmail.com>
2021-03-27 23:46:23 +0800

    Remove unused dependencies.

--- src/app/features/home/camera/camera.module.ts
+++ src/app/features/home/camera/camera.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { NgxIonicImageViewerModule } from 'ngx-ionic-image-viewer';
 import { SharedModule } from '../../../shared/shared.module';
 import { CameraPageRoutingModule } from './camera-routing.module';
 import { CameraPage } from './camera.page';
 
 @NgModule({
-  imports: [SharedModule, CameraPageRoutingModule, NgxIonicImageViewerModule],
+  imports: [SharedModule, CameraPageRoutingModule],
   declarations: [CameraPage],
 })
 export class CameraPageModule {}
--- src/app/features/home/photo/photo.module.ts
+++ src/app/features/home/photo/photo.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
-import { NgxIonicImageViewerModule } from 'ngx-ionic-image-viewer';
 import { SwiperModule } from 'swiper/angular';
 import { SharedModule } from '../../../shared/shared.module';
 import { PhotoPageRoutingModule } from './photo-routing.module';
 import { PhotoPage } from './photo.page';
 
 @NgModule({
-  imports: [
-    SharedModule,
-    PhotoPageRoutingModule,
-    NgxIonicImageViewerModule,
-    SwiperModule,
-  ],
+  imports: [SharedModule, PhotoPageRoutingModule, SwiperModule],
   declarations: [PhotoPage],
 })
 export class PhotoPageModule {}
